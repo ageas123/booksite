4ef7d717b861e7a4e4b6fa2b49b0f2157ea5f0ef
ageas123 <andrej2802s@gmail.com>
2023-12-10 10:03:50 +0300

    book description

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 function myFunction(){
     document.getElementsByClassName("div1")[0].style.backgroundColor = "blueviolet";
 }
+
+
+    let acc = document.getElementsByClassName("accordion");
+    let panelArray = document.getElementsByClassName("panel");
+    let i;
+
+    for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let panel = panelArray[0];
+        let compStyles = window.getComputedStyle(panel);
+        if (compStyles.getPropertyValue("max-height") == "0px") {
+        panel.style.maxHeight = "100%";
+        } else {
+        panel.style.maxHeight = "0px";
+        }
+    });
+    }
